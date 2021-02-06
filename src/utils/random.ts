76a8de2b38804de33f260bf10dfe1b18c109c73c
Roman Lukashik <roman.lukashik@gmail.com>
2021-02-06 19:55:43 +0300

    Code refactoring

--- src/utils/random.ts
+++ src/utils/random.ts
@@ -2,20 +2,22 @@ const MIN = -(2 ** 31)
 const MAX = (2 ** 31) - 1
 
 export const random = (key: string) => {
-  return <T extends any>(arr: T[]): T => arr[integer(xorshift(hash(key)), 0, arr.length - 1)]
+  return <T extends any>(arr: T[]): T => arr[integer(xorshift32(hash(key)), 0, arr.length - 1)]
 }
 
 const integer = (value: number, min: number, max: number) =>
   Math.floor(((value - MIN) / (MAX - MIN)) * (max + 1 - min) + min)
 
-const hash = (key: string) =>
-  key.split('')
-    .map((x) => x.charCodeAt(0))
-    .reduce((hash, x) => xorshift(((hash << 5) - hash + x) | 0), 0)
-
-const xorshift = (value: number) =>
+const xorshift32 = (value: number) =>
   shiftLeft(shiftRight(shiftLeft(value, 13), 17), 5)
 
 const shiftLeft = (value: number, shift: number) => value ^ (value << shift)
 
 const shiftRight = (value: number, shift: number) => value ^ (value >> shift)
+
+const hash = (key: string) =>
+  key.split('')
+    .map((x) => x.charCodeAt(0))
+    .reduce((hash, x) => toInt32((hash << 5) - hash + x), 0)
+
+const toInt32 = (x: number) => x | 0
