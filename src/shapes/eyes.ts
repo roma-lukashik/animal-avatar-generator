c5f88d0c43ee00ba522819ff6db5dceed549e413
Roman Lukashik <roman.lukashik@gmail.com>
2021-07-31 01:10:04 +0300

    Replace black color with #15212a

--- src/shapes/eyes.ts
+++ src/shapes/eyes.ts
@@ -2,55 +2,55 @@ import { Shape } from './types'
 
 export const eyes: Shape[] = [
   (_color) => `
-    <circle cx="169" cy="250" r="20.4"/>
+    <circle cx="169" cy="250" r="20.4" fill="#15212a"/>
     <circle cx="175.4" cy="255.8" r="9.6" fill="#fff"/>
     <circle cx="159.4" cy="241" r="4.2" fill="#fff"/>
-    <circle cx="331" cy="250" r="20.4"/>
+    <circle cx="331" cy="250" r="20.4" fill="#15212a"/>
     <circle cx="324.6" cy="255.8" r="9.6" fill="#fff"/>
     <circle cx="340.6" cy="241" r="4.2" fill="#fff"/>
   `,
   (_color) => `
-    <circle cx="169" cy="254.2" r="18.9"/>
+    <circle cx="169" cy="254.2" r="18.9" fill="#15212a"/>
     <circle cx="175.9" cy="258.3" r="6.3" fill="#fff"/>
-    <circle cx="331" cy="254.2" r="18.9"/>
+    <circle cx="331" cy="254.2" r="18.9" fill="#15212a"/>
     <circle cx="324.1" cy="258.3" r="6.3" fill="#fff"/>
   `,
   (_color) => `
-    <circle cx="169" cy="254.5" r="17.2"/>
+    <circle cx="169" cy="254.5" r="17.2" fill="#15212a"/>
     <circle cx="175.3" cy="247.8" r="5.9" fill="#fff"/>
-    <circle cx="331" cy="254.5" r="17.2"/>
+    <circle cx="331" cy="254.5" r="17.2" fill="#15212a"/>
     <circle cx="324.7" cy="247.8" r="5.9" fill="#fff"/>
   `,
   (_color) => `
-    <circle cx="331.04" cy="253.21" r="22.21"/>
+    <circle fill="#15212a" cx="331.04" cy="253.21" r="22.21"/>
     <circle fill="#fff" cx="338.81" cy="246.07" r="8.97"/>
     <circle fill="#fff" cx="322.5" cy="263.97" r="5.01"/>
-    <circle cx="168.96" cy="253.21" r="22.21"/>
+    <circle fill="#15212a" cx="168.96" cy="253.21" r="22.21"/>
     <circle fill="#fff" cx="161.19" cy="246.07" r="8.97"/>
     <circle fill="#fff" cx="177.5" cy="263.97" r="5.01"/>
   `,
   (_color) => `
-    <circle cx="168.62" cy="248.28" r="15.49"/>
+    <circle fill="#15212a" cx="168.62" cy="248.28" r="15.49"/>
     <circle fill="#fff" cx="163.62" cy="242.97" r="4.95"/>
-    <circle cx="331.04" cy="248.28" r="15.49"/>
+    <circle fill="#15212a" cx="331.04" cy="248.28" r="15.49"/>
     <circle fill="#fff" cx="336.04" cy="242.97" r="4.95"/>
   `,
   (_color) => `
-    <path d="M339.3 256.8c0 15.3-8 15.1-17.9 15.1-9.8 0-17.8.2-17.8-15 0-15.4 8-27.8 17.8-27.8 9.9 0 17.9 12.4 17.9 27.7z"/>
+    <path fill="#15212a" d="M339.3 256.8c0 15.3-8 15.1-17.9 15.1-9.8 0-17.8.2-17.8-15 0-15.4 8-27.8 17.8-27.8 9.9 0 17.9 12.4 17.9 27.7z"/>
     <ellipse cx="329.2" cy="241.9" fill="#fff" rx="5.1" ry="7.5" transform="rotate(-22 329.2 242)"/>
-    <path d="M196.5 256.8c0 15.3-8 15.1-18 15.1s-17.8.2-17.8-15c0-15.4 8-27.8 17.9-27.8s17.9 12.4 17.9 27.7z"/>
+    <path fill="#15212a" d="M196.5 256.8c0 15.3-8 15.1-18 15.1s-17.8.2-17.8-15c0-15.4 8-27.8 17.9-27.8s17.9 12.4 17.9 27.7z"/>
     <ellipse cx="186.4" cy="241.9" fill="#fff" rx="5.1" ry="7.5" transform="rotate(-22 186.4 242)"/>
   `,
   (_color) => `
-    <circle cx="180.11" cy="258.32" r="17.11"/>
+    <circle fill="#15212a" cx="180.11" cy="258.32" r="17.11"/>
     <circle fill="#fff" cx="174.66" cy="252.88" r="5.44"/>
-    <circle cx="320.02" cy="258.32" r="17.11"/>
+    <circle fill="#15212a" cx="320.02" cy="258.32" r="17.11"/>
     <circle fill="#fff" cx="314.58" cy="252.88" r="5.44"/>
   `,
   (_color) => `
-    <circle cx="168.96" cy="262.77" r="18.38"/>
+    <circle fill="#15212a" cx="168.96" cy="262.77" r="18.38"/>
     <circle fill="#fff" cx="163.12" cy="256.93" r="5.84"/>
-    <circle cx="331.04" cy="262.77" r="18.38"/>
+    <circle fill="#15212a" cx="331.04" cy="262.77" r="18.38"/>
     <circle fill="#fff" cx="325.2" cy="256.93" r="5.84"/>
   `,
 ]
