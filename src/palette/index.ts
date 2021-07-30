1a426472406e4d653507562819c83f352a7daf80
Roman Lukashik <roman.lukashik@gmail.com>
2021-07-31 01:12:41 +0300

    Change palette

--- src/palette/index.ts
+++ src/palette/index.ts
@@ -1,14 +1,16 @@
 export const backgroundColors = [
+  '#fcf7d1',
   '#ece2e1',
+  '#e4e3cd',
+  '#c4ddd6',
+  '#b5f4bc',
 ]
 
 export const avatarColors = [
-  '#d291bc',
-  '#ea8a8a',
-  '#ee9595',
-  '#fa9579',
-  '#b1bed5',
-  '#a0c1b8',
-  '#7e8a97',
-  '#9dad7f',
+  '#d7b89c',
+  '#b18272',
+  '#EC8A90',
+  '#a1Ac88',
+  '#99c9bd',
+  '#50c8c6',
 ]
